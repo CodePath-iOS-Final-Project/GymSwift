564b517a52938837304652d24756944028bce0fe
Rebecca Li <Rebecca.CYLi@gmail.com>
2021-11-30 13:20:27 -0500

    Posts details and comments features

--- GymSwift/GymSwift/Posts/PostDetailsViewController.swift
+++ GymSwift/GymSwift/Posts/PostDetailsViewController.swift
@@ -1,40 +1,43 @@
 //
-//  FeedViewController.swift
+//  PostDetailsViewController.swift
 //  GymSwift
 //
-//  Created by Rebecca Li on 11/19/21.
+//  Created by Rebecca Li on 11/30/21.
 //
 
 import UIKit
 import Parse
 import AlamofireImage
 
-class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+class PostDetailsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var tableView: UITableView!
     
     var posts = [PFObject]()
+    var selectedPost: PFObject!
+    
     var numberOfPosts: Int!
     
     let myRefreshControl = UIRefreshControl()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         tableView.delegate = self
         tableView.dataSource = self
-        
-        tableView.rowHeight = UITableView.automaticDimension;
-        tableView.estimatedRowHeight = UITableView.automaticDimension;
-        
-        myRefreshControl.addTarget(self, action: #selector(viewDidAppear(_:)), for: .valueChanged)
-        tableView.refreshControl = myRefreshControl
+
+        // Do any additional setup after loading the view.
     }
     
+    //    <Posts: 0x600001c6b600, objectId: cXGocYrqUR, localId: (null)> {
+    //        author = "<PFUser: 0x60000165dc00, objectId: xrEnSqTvm4, localId: (null)>";
+    //        image = "<PFFileObject: 0x600003715080>";
+    //        post = "Nof8. 3 fnoef9s0. F9e f 2093u f9efjs err 3 3 f vivid 0se8v83 ";
+    //    }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        numberOfPosts = 20
+        numberOfPosts = 1
         let query = PFQuery(className: "Posts")
         query.includeKey("author")
         query.limit = numberOfPosts
@@ -48,41 +51,22 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
     }
     
-    func loadMorePosts(){
-        let query = PFQuery(className: "Posts")
-        query.includeKey("author")
-        query.limit = numberOfPosts + 20
-        
-        query.findObjectsInBackground {(posts, error) in
-            if posts != nil {
-                self.posts = posts!
-                self.tableView.reloadData()
-            }
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        if indexPath.row + 1 == posts.count{
-            loadMorePosts()
-        }
-    }
-    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
     }
-    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
-        let post = posts[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostDetailsCommentCell") as! PostDetailsCommentCell
+        let post = selectedPost!
         let user = post["author"] as! PFUser
         
-        cell.usernameLabel.text = user.username
-        cell.postsCaptionLabel.text = post["post"] as? String
+        cell.usernameTopLabel.text = user.username
+        cell.usernameBottomLabel.text = user.username
+        cell.captionLabel.text = (post["post"] as! String)
         
         let tryImageOpen = post["image"]
         if tryImageOpen as! NSObject == NSNull() {
@@ -92,24 +76,21 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
             let urlString = imageFile.url!
             let url = URL(string: urlString)!
     
-            cell.photoView.af.setImage(withURL: url)
+            cell.photoImage.af.setImage(withURL: url)
     
             return cell
         }
     }
+    
+
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-        let cell = sender as! UITableViewCell
-        let indexPath = tableView.indexPath(for: cell)!
-        
-        let post = posts[indexPath.row]
-        let postDetailsViewController = segue.destination as! PostDetailsViewController
-        postDetailsViewController.selectedPost = post
-        
-        tableView.deselectRow(at: indexPath, animated: true)
     }
+    */
+
 }
