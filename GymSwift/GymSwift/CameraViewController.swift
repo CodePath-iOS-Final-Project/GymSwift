9e7a830a873014ad2eb425806bc6bd2e82960195
nirvanapersaud <nirvanapersaud88@gmail.com>
2021-11-30 18:00:24 -0500

    Profile/Journal

---
+++ GymSwift/GymSwift/CameraViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CameraViewController.swift
+//  GymSwift
+//
+//  Created by Nirvana Persaud  on 11/30/21.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var onSubmit: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GymSwift/GymSwift/ProfileViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileViewController.swift
+//  GymSwift
+//
+//  Created by Nirvana Persaud  on 11/28/21.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var nameField: UILabel!
+    @IBOutlet weak var customStatusField: UILabel!
+    @IBOutlet weak var aboutMeField: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
