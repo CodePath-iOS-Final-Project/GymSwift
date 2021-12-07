62736b29c4ceb979e9d5ec7de59dbc57341323c5
philipjpark <philipparknyc@gmail.com>
2021-12-06 22:40:48 -0500

    Added nearby park locations

--- GymSwift/GymSwift/MapViewController.swift
+++ GymSwift/GymSwift/MapViewController.swift
@@ -19,7 +19,6 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         createLocationButton()
        
     }
@@ -28,8 +27,10 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
             let locationButton = CLLocationButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
             locationButton.center = CGPoint(x: view.center.x, y: view.frame.size.height-110)
             locationButton.label = .currentLocation
-            locationButton.icon = .arrowOutline
+            locationButton.icon = .arrowFilled
             locationButton.cornerRadius = 25.0
+            locationButton.layer.shadowOpacity = 0.25
+            locationButton.alpha = 0.78
             view.addSubview(locationButton)
             locationButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
         }
@@ -51,26 +52,46 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first {
             manager.stopUpdatingLocation()
-            
             render(location)
         }
     }
     
     func render(_ location:  CLLocation ){
-        
-        let coordinate =  CLLocationCoordinate2D(latitude: location.coordinate.latitude,
-                                                 longitude: location.coordinate.longitude)
-        
+        let coordinate =  CLLocationCoordinate2D(latitude: 40.7366,
+                                                 longitude: -73.8201)
         let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-        
         let region = MKCoordinateRegion(center: coordinate,
                                         span: span)
         mapView.setRegion(region,
                           animated: true)
+        nearbyParks()
+    }
+    
+    func nearbyParks() {
+        var region = MKCoordinateRegion()
+        region.center = CLLocationCoordinate2D(latitude: 40.7366,
+                                            longitude: -73.8201)
         
-        let pin = MKPointAnnotation()
-        pin.coordinate = coordinate
-        mapView.addAnnotation(pin)
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "parks"
+        request.region = region
+        
+        let search = MKLocalSearch(request: request)
+        search.start { ( response, error ) in
+            guard let response = response else {
+            return
+        }
+
+        for item in response.mapItems {
+            let pins = MKPointAnnotation()
+            pins.coordinate = item.placemark.coordinate
+            pins.title = item.name
+
+            DispatchQueue.main.async{
+                self.mapView.addAnnotation(pins)
+                }
+            }
+        }
     }
 }
 
